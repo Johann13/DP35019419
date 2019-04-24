77747ba97441c667a3db64b9bf62d2e3e33fd828
Johann Feser <johannfeser@gmail.com>
2019-04-24 12:40:15 +0200

    split tests for ha01

--- src/test/java/ha01/DeliveryTest.java
+++ src/test/java/ha01/DeliveryTest.java
@@ -9,7 +9,21 @@ import org.junit.Test;
 public class DeliveryTest {
 
 	@Test
-	public void test1() {
+	public void task1() {
+		//Create DeliveryService with a Caterer
+		DeliveryService deliveryService = new DeliveryService(new BurgerShop());
+		//deliver and print the result
+		deliveryService.deliver("M4", "My Home Address");
+
+		//swap the Caterer and print the result
+		deliveryService.setSubContractor(new PizzaShop());
+		deliveryService.deliver("M4", "My other Address");
+		deliveryService.setSubContractor(new DoenerShop());
+		deliveryService.deliver("M4", "My third Address");
+	}
+
+	@Test
+	public void task2() {
 		//Create DeliveryService with a Caterer
 		DeliveryService deliveryService = new DeliveryService(new BurgerShop());
 		//deliver and print the result
@@ -24,7 +38,6 @@ public class DeliveryTest {
 		//test newly added AsiaShop
 		deliveryService.setSubContractor(new AsiaShop());
 		deliveryService.deliver("M4", "My new Address");
-
 	}
 
 }
