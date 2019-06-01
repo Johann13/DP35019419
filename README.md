c675a78bed34a955b87134610e77530d149df5f3
Johann Feser <johannfeser@gmail.com>
2019-06-01 17:27:26 +0200

    update readme

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # DP35019419
 Design Pattern Homework by Johann Feser - 35019419
 
-- HA01 Strategy Pattern         7/10
-- HA02 Visitor Pattern          8/10
-- HA03 Chain of Responsibility   /10
-- HA04 Listener Pattern          /10
-- HA05
-- HA06
+- HA01 Strategy Pattern          7/10
+- HA02 Visitor Pattern           8/10
+- HA03 Chain of Responsibility   8/10
+- HA04 Listener Pattern         10/10
+- HA05 ---
+- HA06 Prototypen
 - HA07
 - HA08
 - HA09
---
+++ src/test/java/ha06/DrawTests.java
@@ -0,0 +1,70 @@
+package ha06;
+
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+
+import static org.testfx.api.FxAssert.verifyThat;
+
+public class DrawTests extends ApplicationTest {
+
+
+	@Before
+	public void before() throws Exception {
+		ApplicationTest.launch(Main.class);
+
+	}
+
+	@Override
+	public void start(Stage stage) {
+		stage.show();
+	}
+
+	@Test
+	public void drawHouses() {
+
+		String[] list = new String[]{"line a 20 20 30 10", "line b 40 20 30 10", "line c 20 40 20 20", "line d 40 20 40 40", "line e 40 40 20 40", "line f 40 40 20 20", "line g 20 40 40 20", "line h 20 20 40 20", "group h1 a b c d e f g h", "clone h1 h2 50 0", "clone h1 h3 0 50"};
+
+		for (String s : list) {
+			textClick();
+			textEquals("");
+			write(s);
+			textEquals(s);
+			buttonClick();
+			textEquals("");
+		}
+
+		sleep(4000);
+
+
+	}
+
+	private void textEquals(String text) {
+		verifyThat("#textField", (TextField t) -> {
+			return t.getText().equals(text);
+		});
+	}
+
+	private void buttonClick() {
+		clickOn("#button");
+	}
+
+	private void textClick() {
+		clickOn("#textField");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		FxToolkit.hideStage();
+		release(new KeyCode[]{});
+		release(new MouseButton[]{});
+	}
+
+
+}
